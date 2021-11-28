c50add7233ef4aff0f15e030928db00d0e769754
anhthai <hoangtrinhanhthai@gmail.com>
2021-11-28 23:58:41 +0700

    make chat for fe

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,9 +6,15 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import store from './store';
 import 'vue2-datepicker/index.css';
+import VueSocketIO from 'vue-socket.io'
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+
 Vue.config.productionTip = false;
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://localhost:4000',
+}))
 
 new Vue({
   render: (h) => h(App),
